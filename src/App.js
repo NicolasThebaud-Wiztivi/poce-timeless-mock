a1d7ea8349fdee6d003cd58313ede4010776a15f
Nicolas Thebaud <nicolas.thebaud@wiztivi.com>
2021-01-07 13:55:47 +0100

    remove watch + update App.js data format

--- src/App.js
+++ src/App.js
@@ -4,51 +4,40 @@ import useThemeMap from "./ThemeMapperAPI";
 import axios from 'axios';
 
 function App() {
-    // const [tn, setTn] = useState(0);
-    const [colors, setColors] = useState({});
+    const [tid, setTid] = useState();
+
     const [customerTheme, setCustomerTheme] = useState({});
     const [getCustomerTheme] = useThemeMap();
-
-    useEffect(() => {
-        // TODO: update theme number in remote HERE
-
-        const fetchData = async () => {
-            const customerThemeRaw = await getCustomerTheme();
-            console.warn('×', customerThemeRaw);
-            setColors(customerThemeRaw.style.colors);
-            setCustomerTheme(customerThemeRaw.style.colors);
-        };
-
-        fetchData();
-    }, []);
 
 
     // Loop every second to simulate socket listening to updates
     useEffect(() => {
         const interval = setInterval(() => {
             const fetchData = async () => {
-                const colorList = (await axios.get('http://localhost:4444/state')).data;
-                console.warn('×', colorList);
-                console.warn('××', customerTheme);
-                console.warn('×××', Object.assign({}, customerTheme, colorList));
-                Object.keys(colorList).length && setColors(Object.assign({}, customerTheme, colorList));
+                const customerThemeRaw = (await getCustomerTheme());
+
+                setTid(customerThemeRaw.id);
+                setCustomerTheme(customerThemeRaw.style);
             };
 
             fetchData();
-        }, 3000);
+        }, 500);
         return () => clearInterval(interval);
     }, [customerTheme]);
 
     return (
-        <div className="App" style={{'backgroundColor': colors.$COLOR_LIGHT}}>
-            <h1 className="$COLOR_DARK" style={{'color': colors.$COLOR_DARK}}>
+        <div className="App" style={{'backgroundColor': customerTheme.$COLOR_LIGHT}}>
+            <p> theme id n° {tid} </p>
+            <h1 className="$COLOR_DARK" style={{'color': customerTheme.$COLOR_DARK}}>
                 Lorem Ipsum dolor sit amet, consectetur adipiscing elit. sed non risus. [$COLOR_DARK]</h1>
-            <h3 className="$COLOR_GREY_4" style={{'color': colors.$COLOR_GREY_4}}>
+            <h3 className="$COLOR_GREY_4" style={{'color': customerTheme.$COLOR_GREY_4}}>
                 Lorem Ipsum dolor sit amet, consectetur adipiscing elit. sed non risus. [$COLOR_GREY4]</h3>
-            <h3 className="$COLOR_GREY_5" style={{'color': colors.$COLOR_GREY_5}}>
+            <h3 className="$COLOR_GREY_5" style={{'color': customerTheme.$COLOR_GREY_5}}>
                 Lorem Ipsum dolor sit amet, consectetur adipiscing elit. sed non risus. [$COLOR_GREY5]</h3>
-            <p className="$COLOR_GREY_5" style={{'color': colors.$COLOR_GREY_5}}>
+            <p className="$COLOR_GREY_5" style={{'color': customerTheme.$COLOR_GREY_5}}>
                 Lorem Ipsum dolor sit amet, consectetur adipiscing elit. sed non risus. [$COLOR_GREY5]</p>
+
+            <button style={{'textTransform': customerTheme.$BUTTON_CASE}}>button</button>
         </div>
     );
 }
